826b1443787d6e86090a070e800cd94cdee4634a
maengseonu <aodtjsdn123@gmail.com>
2022-01-07 23:07:43 +0900

    Protecting Resolvers

--- users/users.utils.js
+++ users/users.utils.js
@@ -1,4 +1,4 @@
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export const getUser = async (token) => {
@@ -6,7 +6,7 @@ export const getUser = async (token) => {
     if (!token) {
       return null;
     }
-    const { id } = await jsonwebtoken.verify(token, process.env.SECRET_KEY);
+    const { id } = await jwt.verify(token, process.env.SECRET_KEY);
     const user = await client.user.findUnique({ where: { id } });
     if (user) {
       return user;
@@ -17,3 +17,15 @@ export const getUser = async (token) => {
     return null;
   }
 };
+
+export function protectedResolver(ourResolver) {
+  return function (root, args, context, info) {
+    if (!context.loggedInUser) {
+      return {
+        ok: false,
+        error: "Please log in to perform this action.",
+      };
+    }
+    return ourResolver(root, args, context, info);
+  };
+}

5708322dd0839626c5f7f5f81cd4d0da2626f775
maengseonu <aodtjsdn123@gmail.com>
2022-01-10 14:12:52 +0900

    Changing Avatar

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { getUser, protectResolver } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 
-const server = new ApolloServer({
+const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   context: async ({ req }) => {
@@ -20,7 +20,8 @@ const server = new ApolloServer({
 
 const app = express();
 app.use(logger("tiny"));
-server.applyMiddleware({ app });
+apollo.applyMiddleware({ app });
+app.use("/static", express.static("uploads"));
 app.listen({ port: PORT }, () => {
   console.log(`🚀Server is running on http://localhost:${PORT} ✅`);
 });

0a5e314f836426036bb9182b5786c4bea7f3680c
maengseonu <aodtjsdn123@gmail.com>
2022-01-10 22:36:50 +0900

    Unfollow User and See Followers

--- users/unfollowUser/unfollowUser.typeDefs.js
+++ users/unfollowUser/unfollowUser.typeDefs.js
@@ -1,11 +1,11 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type FollowUserResult {
-    ok: Boolean!
+  type UnfollowUserResult {
+    ok: Boolean
     error: String
   }
   type Mutation {
-    followUser(username: String!): FollowUserResult
+    unfollowUser(username: String!): UnfollowUserResult
   }
 `;

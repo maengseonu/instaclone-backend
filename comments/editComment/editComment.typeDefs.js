96052a045bb5feb4ba43ffb935a70167876e0f77
maengseonu <aodtjsdn123@gmail.com>
2022-01-12 20:05:34 +0900

    editComment

--- comments/editComment/editComment.typeDefs.js
+++ comments/editComment/editComment.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-express";
 
 export default gql`
   type Mutation {
-    createComment(photoId: Int!, payload: String!): MutationResponse!
+    editComment(id: Int!, payload: String!): MutationResponse!
   }
 `;
